f72720f58db941f68db65368813759ed75f1e37a
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-23 15:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function doSomething() {
+  let age = document.getElementById("age").value;
+  let name = document.getElementById("name").value;
+
+  if (age == "" || name == "") {
+    alert("Please enter valid details");
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve("Welcome, . You can vote.");
+      } else {
+        reject("Oh sorry . You aren't old enough.");
+      }
+    }, 1000);
+  });
+}
